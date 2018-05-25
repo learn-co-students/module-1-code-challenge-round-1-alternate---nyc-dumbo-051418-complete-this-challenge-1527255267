72232b3679506feb94f8335169fc04f31b424afe
github username <github email address>
2018-05-25 10:37:34 -0400

    solutions

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 class Article
 
+  attr_accessor :author, :magazine
+  @@all = []
+
+  def initialize(author, magazine)
+    @author = author
+    @magazine = magazine
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,33 @@
 class Magazine
   attr_accessor :name, :category
+  @@all = []
 
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
-end
+  def self.find_by_name(name)
+     # given a string of magazine name, returns the first magazine that matches
+     # iterate through all of the instances of magazine
+     # return the first magazine that matches
+     self.all.select do |magazine|
+       magazine.name.first
+     end
+  end
+
+  def article_titles
+     # returns a list of the titles of all articles written for that magazine
+     # iterate through articles
+     # find articles written for that magazine
+     Article.all.select do |article|
+       article.magazine == self
+     end
+   end
+ end
